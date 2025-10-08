8e50acc6ceb081128d2c8f381676019a62de49f8
LinoLakaca <lino.lakaca@gmail.com>
2025-10-08 09:50:39 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -13,13 +13,22 @@ def ucitaj_tekst(filepath):
 def ocisti_tekst(tekst):
 #Kod za pročišćavanje teksta ide ovdje
     tekst = tekst.lower()
-    interpunkcija = ['.',',','!','?',':',';','"',"'",'(',')']
+    interpunkcija = ['.', ',', '!', '?', ':', ';', '"', "'", '(', ')']
     for znak in interpunkcija:
         tekst = tekst.replace(znak, '')
-    
+
     lista_rijeci = tekst.split()
+    
     return lista_rijeci
-    return tekst
+def broji_rijeci(lista_rijeci):
+    #rječnik u koji ćemo spremiti svaku riječ i koliko se puta ta riječ ponovila u tekstu
+    brojac_rijeci = {}
+    for rijec in lista_rijeci:
+        if rijec in brojac_rijeci:
+            brojac_rijeci[rijec] += 1
+        else:
+            brojac_rijeci[rijec] = 1
+    return brojac_rijeci
        
 if __name__=="__main__":
     filepath = "tekst.txt"
@@ -30,9 +39,15 @@ if __name__=="__main__":
         print(ucitani_tekst)
     else:
         print("Greška pri učitavanju datoteke.")
+
     ucitani_tekst = ocisti_tekst(ucitani_tekst)
+
     if ucitani_tekst:
         print("Očišćeni tekst je:")
         print(ucitani_tekst)
+        #brojanje riječi i ispis rječnika
+        brojac_rijeci = broji_rijeci(ucitani_tekst)
+        print("Broj riječi u tekstu:")
+        print(brojac_rijeci)  
     else:
-        print("Greška pri očišćavanju teksta.")
+        print("Greška pri čišćenju teksta.")
